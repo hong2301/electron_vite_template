c22a2697ef153fb0cfe8f12b56dda115f1986a5f
疯了个疯 <honononong@icloud.com>
2025-09-19 15:29:05 +0800

    feat(): layout布局

--- src/renderer/src/components/layout.vue
+++ src/renderer/src/components/layout.vue
@@ -1,12 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { WarnTriangleFilled } from '@element-plus/icons-vue'
+</script>
 
 <template>
   <div class="overture">
-    <div class="head"></div>
-    <div class="body">
-      <router-view />
+    <div class="content">
+      <div class="tail"></div>
+      <div class="body">
+        <div class="head">
+          <div class="logBox">
+            <img class="log" src="../assets//log.png" />
+            <div class="w-title">自助拍照机</div>
+          </div>
+          <div class="desBox">
+            <el-icon class="w-label-des" style="font-size: 1.3rem; margin-right: 0.2rem"
+              ><WarnTriangleFilled
+            /></el-icon>
+            <div class="w-label-des">操作过程如有问题请联系引导员或到前台办理</div>
+          </div>
+        </div>
+        <div class="page">
+          <router-view />
+        </div>
+      </div>
     </div>
-    <div class="tail"></div>
   </div>
 </template>
 
@@ -14,17 +31,65 @@
 .overture {
   width: 100vw;
   height: 100vh;
+  background: linear-gradient(to right, rgb(37, 56, 148), rgb(43, 27, 167), rgb(37, 56, 148));
+  padding: 0.4rem;
+}
+.content {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+.body {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 92%;
+  background-color: white;
+  border-radius: 1rem 1rem 2.5rem 2.5rem;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.5);
 }
 .head {
   width: 100%;
-  height: 10%;
+  height: 4rem;
+  background-color: rgb(246, 248, 254);
+  border-radius: 1rem;
+  display: flex;
 }
-.body {
+.page {
   width: 100%;
-  height: 80%;
+  flex: 1;
+  border-radius: 0rem 0rem 2.5rem 2.5rem;
 }
 .tail {
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
-  height: 10%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.15);
+  border-radius: 1rem;
+}
+.logBox {
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding-left: 2.5rem;
+  box-sizing: border-box;
+}
+.desBox {
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  justify-content: flex-end;
+  padding-right: 2rem;
+}
+.log {
+  height: 2.5rem;
 }
 </style>
